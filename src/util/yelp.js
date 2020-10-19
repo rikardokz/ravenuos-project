33b6d7923b9aaab8990e51cb8065de6b078d0453
Ricardo <rikardo_kz@live.com.pt>
2020-10-19 19:37:29 +0100

    add util yelp.js to connect with yelp api

---
+++ src/util/yelp.js
@@ -0,0 +1,38 @@
+/* Client ID
+zXlgTdxo2LPabmlzb7KdTw
+
+API Key
+7RjKR_ODS5Hl1_p5aG5WTfHvk58z1a0wdOxm5PouhHzs20-77BWmysrvBAmc1v-65K8hhPQY8zXKk5EXWD9bt1J2WMuE8JJeN5WsA1v8DxhXBezXy1STMy3f5ryEccW8 */
+
+const apiKey = "7RjKR_ODS5Hl1_p5aG5WTfHvk58z1a0wdOxm5PouhHzs20-77BWmysrvBAmc1v-65K8hhPQY8zXKk5EXWD9bt1J2WMuE8JJeN5WsA1v8DxhXBezXy1STMy3ryEccW8";
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: { Authorization: `Bearer ${apiKey}`}
+        }).then(response => { 
+            return response.json()
+        }).then(jsonResponse => {
+            if(jsonResponse.business) {
+                jsonResponse.business.map(business => {
+                      return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories,
+                        rating: business.rating,
+                        reviewCount: business.review_count,
+                      }  
+                })
+            } else {
+                throw new Error('Business does not exist')
+            }
+        })
+    }
+}
+
+export default Yelp;
